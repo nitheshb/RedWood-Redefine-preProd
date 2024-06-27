6bb1df46f9f77dd22629c0f769ffbb8dde63adf8
Nithesh <nithe.nithesh@gmail.com>
2024-06-27 17:00:59 +0530

    added size

--- web/src/components/A_CrmModule/CrmPaymentSummary.tsx
+++ web/src/components/A_CrmModule/CrmPaymentSummary.tsx
@@ -14,18 +14,22 @@ const CrmPaymentSummary = ({ selCustomerPayload }) => {
       <>
         <div className="flex flex-col bg-white shadow rounded-md my-1  px-2  pt-2 min-w-[260px]">
           <div className="flex flex-row justify-between tracking-wide mx-">
-            <h6 className="font-bodyLato font-semibold text-xs m-1 mb-2 text-[23px] flex flex-col">
-            <span className="text-[#637381] tracking-wide font-thin">Balance:</span>  {' '} <span className="tracking-wide  font-semibold text-[16px]"> ₹{selCustomerPayload?.T_elgible_balance?.toLocaleString('en-IN')}</span>
+
+            <h6 className="font-bodyLato font-semibold text-xs m-1 flex flex-col">
+            <span className="tracking-wide  font-semibold text-[16px]">
+            ₹{selCustomerPayload?.T_elgible_balance?.toLocaleString('en-IN')}</span><span className="text-[#637381] tracking-wide font-thin">Stage Balance</span>
 
             </h6>
             <section className="flex flex-row">
               {/* <h6 className="font-bodyLato font-semibold text-xs m-1 mb-2">
                 {selCustomerPayload?.T_elgible?.toLocaleString('en-IN')}
               </h6> */}
-              <h6 className="font-bodyLato font-semibold text-xs m-1 mb-2 flex flex-col text-right">
-            <span className="text-[#637381] tracking-wide font-thin">Paid</span> {' '} ₹{(selCustomerPayload?.T_review || 0 + selCustomerPayload?.T_approved || 0 )?.toLocaleString('en-IN')}
 
-              </h6>
+              <h6 className="font-bodyLato font-semibold text-xs m-1 flex flex-col text-right">
+    ₹
+              ₹{(selCustomerPayload?.T_review || 0 + selCustomerPayload?.T_approved || 0 )?.toLocaleString('en-IN')}
+              <span className="text-[#637381] tracking-wide font-thin">Paid</span>
+            </h6>
             </section>
           </div>
           <div className="flex flex-row mx-1">
