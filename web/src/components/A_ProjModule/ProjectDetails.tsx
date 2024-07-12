25559acde69482150578dbb29ae8c3b10a2d1761
Nithesh <nithe.nithesh@gmail.com>
2024-07-12 08:05:56 +0530

    added add block

--- web/src/components/A_ProjModule/ProjectDetails.tsx
+++ web/src/components/A_ProjModule/ProjectDetails.tsx
@@ -125,8 +125,8 @@ const setLoading1 = (x)=>{
     // setSubmitForm(() => submitFormFunction);
   };
   return (
-      <div className="h-full flex flex-col  py-6  shadow-xl overflow-y-scroll bg-[#57C0D0]">
-      <div className="px-2 sm:px-6  z-10 absolute top-0  w-full  py-2 bg-[#57C0D0]">
+      <div className="h-full flex flex-col  py-6  shadow-xl overflow-y-scroll bg-gradient-to-r from-blue-300 to-cyan-300">
+      <div className="px-2 sm:px-6  z-10 absolute top-0  w-full  py-2 bg-gradient-to-r from-blue-300 to-cyan-300">
         <Dialog.Title className=" font-semibold text-xl mr-auto    tracking-wider text-[14px]">
           Project Information
         </Dialog.Title>
